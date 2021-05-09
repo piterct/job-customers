562efa65a8a56dd9e8f0374b77e911b870dc9c98
Michael Peter <michael_piterct@hotmail.com>
2021-05-09 14:42:04 -0300

    Contents External Services

---
+++ Job.Customers/Job.Customer.ExecuteCustomer/ExternalServices/Contents/HttpResponseExtensios.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Job.Customer.ExecuteCustomer.ExternalServices.Contents
+{
+    public static class HttpResponseExtensios
+    {
+        public static T ContentAsType<T>(this HttpResponseMessage response)
+        {
+            var data = response.Content.ReadAsStringAsync().Result;
+            return string.IsNullOrEmpty(data) ?
+                            default(T) :
+                            JsonConvert.DeserializeObject<T>(data);
+        }
+    }
+}
