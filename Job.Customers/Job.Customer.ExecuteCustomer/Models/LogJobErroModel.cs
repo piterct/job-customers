d671d7c5bd0a47360c9a19b5467872999d7773af
Michael Peter <michael_piterct@hotmail.com>
2021-05-12 21:26:58 -0300

    Errors Model

---
+++ Job.Customers/Job.Customer.ExecuteCustomer/Models/LogJobErroModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Job.Customer.ExecuteCustomer.Models
+{
+    public class LogJobErroModel
+    {
+        public string StackTrace { get; set; }
+        public string TipoException { get; set; }
+        public DateTime OcorreuEm { get; set; }
+        public string Area { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
---
+++ Job.Customers/Job.Customer.ExecuteCustomer/Models/LogJobModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Job.Customer.ExecuteCustomer.Models
+{
+    public class LogJobModel
+    {
+        public DateTime OcorreuEm { get; set; }
+        public string Area { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
