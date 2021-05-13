82bece8e8ce71096ae305c6e00a3ba31ceea2398
Michael Peter <michael_piterct@hotmail.com>
2021-05-13 03:46:47 -0300

    CustomerAPI Endpoint

--- Job.Customers/Job.Customer.ExecuteCustomer/Http/CustomerAPI.cs
+++ Job.Customers/Job.Customer.ExecuteCustomer/Http/CustomerAPI.cs
@@ -1,6 +1,25 @@
-﻿namespace Job.Customer.ExecuteCustomer.Http
+﻿using Job.Customer.ExecuteCustomer.ExternalServices.Contents;
+using Job.Customer.ExecuteCustomer.ExternalServices.HttpRequest;
+using Job.Customer.ExecuteCustomer.Interfaces;
+using Job.Customer.ExecuteCustomer.Models.Response;
+using System.Threading.Tasks;
+
+namespace Job.Customer.ExecuteCustomer.Http
 {
-    public  class CustomerAPI
+    public class CustomerAPI : ICustomerAPI
     {
+        private readonly ICustomerAPISettings _customerAPISettings;
+
+        private int timeOut = 360;
+        public CustomerAPI(ICustomerAPISettings customerAPISettings)
+        {
+            _customerAPISettings = customerAPISettings;
+        }
+        public async ValueTask<CustomersResponse> GetTokenConsigaMaisAsync()
+        {
+            var result = await HttpRequestFactory.Get(string.Concat(_customerAPISettings.ApiBasePath, _customerAPISettings.ApiPath), timeOut, string.Empty);
+
+            return result.ContentAsType<CustomersResponse>();
+        }
     }
 }
---
+++ Job.Customers/Job.Customer.ExecuteCustomer/Models/Response/CustomersResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Job.Customer.ExecuteCustomer.Models.Response
+{
+    public class CustomersResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
