e404602d5a826253cba5ed9401604416fe7c4653
Michael Peter <michael_piterct@hotmail.com>
2021-05-15 18:41:40 -0300

    Suspend Process In Memory

---
+++ Job.Customers/Job.Customer.ExecuteCustomer/Helpers/Memory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Job.Customer.ExecuteCustomer.Helpers
+{
+    public static class Memory
+    {
+        public static void SuspendProcessInMemory()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
